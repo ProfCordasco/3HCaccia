8af1a4c50c9dc12b2c67c64ef1c6620821879a8d
ProfCordasco <161847596+ProfCordasco@users.noreply.github.com>
2024-04-13 18:22:03 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-
+function doLogin() {
+  var username = document.getElementById("username").value;
+  var psw = document.getElementById("password").value;
+  if(username == "" || psw == "") {
+    alert("Username e password sono obbligatori");
+  }
+}
